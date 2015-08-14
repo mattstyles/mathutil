43f2d8c4c712c16c0487fd0dfe5a7c87cbcbe16e
Matt Styles <matt@veryfizzyjelly.com>
2015-08-14 10:53:53 +0100

    #update rotate example

--- examples/rotate.js
+++ examples/rotate.js
@@ -1,13 +1,16 @@
 
-var mathutil = require( '../lib' )
-var Vector2 = mathutil.Vector2
+var Vector2 = require( '../lib' ).Vector2
 
 var canvas = document.querySelector( 'canvas' )
 var ctx = canvas.getContext( '2d' )
 
 var vec = new Vector2( 100, 100 )
 
-ctx.strokeStyle = 'rgb( 100, 100, 255 )'
+function color( value ) {
+    return 'rgb( ' + ( 0xff * value ) + ', 0, 0 )'
+}
+
+
 
 function drawLine( pt ) {
     ctx.beginPath()
@@ -17,7 +20,30 @@ function drawLine( pt ) {
 }
 
 var rad = Math.PI / 180
+var step = 720
 
-for ( var a = 0; a < 360; a += 15 ) {
-    drawLine( vec.rotate( a * rad ) )
+function drawRotation() {
+    for ( var a = 0; a < 360; a += ~~step ) {
+        ctx.strokeStyle = color( a / 360 )
+        drawLine( vec.rotate( a * rad ) )
+    }
 }
+
+function go() {
+    ctx.clearRect( 0, 0, 600, 600 )
+
+    step /= 2
+
+    if ( step < 1 ) {
+        step = 360
+    }
+
+    drawRotation()
+
+    setTimeout( function() {
+        go()
+    }, 100 )
+}
+
+
+setTimeout( go, 100 )
