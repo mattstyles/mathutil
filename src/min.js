0befafa52f1e07148838706077f50cdec15d3100
mattstyles <matt@veryfizzyjelly.com>
2015-08-14 20:21:35 +0100

    #add min and max

---
+++ src/min.js
@@ -0,0 +1,10 @@
+
+export default function min( map ) {
+    let val = 0
+    for ( var i = 0; i < map.length; i++ ) {
+        val = map[ i ] < val
+            ? map[ i ]
+            : val
+    }
+    return val
+}
