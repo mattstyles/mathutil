85c7a22ef0e6bff79f10804a9bc49f8b127ee653
Matt Styles <matt@personalurban.com>
2017-01-17 07:04:10 +0000

    add example while cast

--- examples/cast.js
+++ examples/cast.js
@@ -5,14 +5,27 @@ var r = new Ray(new Vector2(1, 0))
 var origin = [2, 0]
 var magnitude = 10
 var step = 1
-var initial = false
+var initial = true
 
 // Create the cast generator
 var cast = r.cast({
   origin, magnitude, step, initial
 })
+var gen = r.cast({
+  origin, magnitude, step, initial
+})()
 
 // Iterate over the generator, logging positions
 for (let p of cast()) {
   console.log('  **  ', p)
+}
+
+let p = gen.next()
+while (!p.done) {
+  console.log('  ~~  ', p.value)
+  if (p.value[0] === 4) {
+    console.log('done')
+    break
+  }
+  p = gen.next()
 }
