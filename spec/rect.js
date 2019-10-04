6e0ad129a751d2a4a4a3babb8c473877e8dd6ecc
Matt Styles <matt@personalurban.com>
2019-10-04 22:13:10 +0100

    add additional Rect methods

--- spec/rect.js
+++ spec/rect.js
@@ -65,6 +65,83 @@ tape('Rect::translate -- static method', t => {
   t.deepEqual(y.pos, [2, 3, 3, 4], 'Translate moves the rect correctly')
 })
 
+tape('Rect::scale -- instance method', t => {
+  t.plan(5)
+
+  const r1 = Rect.of(0, 0, 2, 2)
+  const s1 = r1.scale(2)
+
+  t.deepEqual(s1.pos, [0, 0, 4, 4], 'Scales up')
+  t.deepEqual(r1.pos, [0, 0, 4, 4], 'Scaling is mutative')
+  t.ok(r1 === s1, 'Scaling returns itself')
+
+  t.deepEqual(s1.scale(0.5).pos, [0, 0, 2, 2], 'Scales down')
+
+  const r2 = Rect.of(2, 2, 5, 5)
+  const expected = Rect.of(2, 2, 8, 8)
+
+  t.deepEqual(r2.scale(2).pos, expected.pos, 'Scale preserves [x1, y1]')
+})
+
+tape('Rect::scale -- static method', t => {
+  t.plan(5)
+
+  const r1 = Rect.of(0, 0, 2, 2)
+  const s1 = Rect.scale(r1, 2)
+
+  t.deepEqual(s1.pos, [0, 0, 4, 4], 'Scales up')
+  t.deepEqual(r1.pos, [0, 0, 2, 2], 'Scaling preserves passed in Rect')
+  t.ok(r1 !== s1, 'Scaling returns a new instance')
+
+  t.deepEqual(Rect.scale(r1, 0.5).pos, [0, 0, 1, 1], 'Scales down')
+
+  const r2 = Rect.of(2, 2, 5, 5)
+  const s2 = Rect.scale(r2, 2)
+  const expected = [2, 2, 8, 8]
+
+  t.deepEqual(s2.pos, expected, 'Scale preserves [x1, y1]')
+})
+
+tape('Rect::constrict -- instance method', t => {
+  t.plan(4)
+
+  const r1 = Rect.of(0, 0, 4, 4)
+  const s1 = r1.constrict(1, 1)
+  const e1 = [1, 1, 3, 3]
+
+  t.deepEqual(r1.pos, e1, 'Constrict constricts')
+  t.ok(r1 === s1, 'Constrict returns an instance')
+
+  const r2 = Rect.of(0, 0, 5, 3)
+  const e2 = [1, 0, 4, 3]
+
+  t.deepEqual(r2.constrict(1, 0).pos, e2, 'Constrict applies to a single axis')
+
+  const r3 = Rect.of(2, 2, 8, 8)
+  const e3 = [0, 0, 10, 10]
+  t.deepEqual(r3.constrict(-2, -2).pos, e3, 'Constrict unconstricts, nice.')
+})
+
+tape('Rect::constricts -- static method', t => {
+  t.plan(4)
+
+  const r1 = Rect.of(0, 0, 6, 4)
+  const c1 = Rect.constrict(r1, 1, 1)
+  const e1 = [1, 1, 5, 3]
+
+  t.deepEqual(c1.pos, e1, 'Constrict constricts')
+  t.notOk(r1 === c1, 'Returns a new instance')
+
+  const r2 = Rect.of(0, 0, 5, 3)
+  const e2 = [1, 0, 4, 3]
+
+  t.deepEqual(Rect.constrict(r2, 1, 0).pos, e2, 'Constrict applies to a single axis')
+
+  const r3 = Rect.of(2, 2, 8, 8)
+  const e3 = [0, 0, 10, 10]
+  t.deepEqual(Rect.constrict(r3, -2, -2).pos, e3, 'Constrict unconstricts, nice.')
+})
+
 tape('Rect::slope', t => {
   t.plan(1)
 
@@ -72,4 +149,34 @@ tape('Rect::slope', t => {
   let v = x.slope()
 
   t.deepEqual(v.pos, [3, 2], 'Diagonal slope is returned')
+})
+
+tape('Rect::equal', t => {
+  t.plan(3)
+
+  const r1 = Rect.of(0, 0, 3, 3)
+  const r2 = Rect.of(0, 0, 3, 3)
+
+  t.ok(r1.equal(r2), 'Equal knows when things are equal')
+  t.ok(r1.equal(0, 0, 3, 3), 'Equal accepts vertex parameters')
+  t.notOk(r1 === r2, 'Test subjects are not the same pointer')
+})
+
+tape('Rect can round its vertices to integers', t => {
+  t.plan(3)
+
+  const r1 = Rect.of(0.6, 0.3, 5.2, 5.9)
+  const e1 = [0, 0, 5, 5]
+
+  t.deepEqual(r1.floor().pos, e1, 'Rounds down all vertices')
+
+  const r2 = Rect.of(0.6, 0.3, 5.2, 5.9)
+  const e2 = [1, 1, 6, 6]
+
+  t.deepEqual(r2.ceil().pos, e2, 'Rounds up all vertices')
+
+  const r3 = Rect.of(0.6, 0.3, 5.2, 5.9)
+  const e3 = [0, 0, 6, 6]
+
+  t.deepEqual(r3.round().pos, e3, 'Rounds down [x1, y1] and up [x2, y2]')
 })
--- src/rect.js
+++ src/rect.js
@@ -26,7 +26,7 @@ export class Rect {
    * Translates the entire rectangle
    * @param x <Float>
    * @param y <Float>
-   * @returns <this>
+   * @returns <Rect>
    */
   static translate (rect, x = 0, y = 0) {
     if (!rect || !(rect instanceof Rect)) {
@@ -42,11 +42,104 @@ export class Rect {
   }
 
   /**
+   * Scales the rect
+   * @param s <Float> scalar
+   * @returns <Rect>
+   */
+  static scale (rect, s) {
+    if (!rect || !(rect instanceof Rect)) {
+      throw new Error('Specify rect to translate')
+    }
+
+    return new Rect(
+      rect.pos[0],
+      rect.pos[1],
+      rect.pos[0] + (rect.width * s),
+      rect.pos[1] + (rect.height * s)
+    )
+  }
+
+  static constrict (rect, x, y) {
+    if (!rect || !(rect instanceof Rect)) {
+      throw new Error('Specify rect to translate')
+    }
+
+    return new Rect(
+      rect.pos[0] + x,
+      rect.pos[1] + y,
+      rect.pos[2] - x,
+      rect.pos[3] - y
+    )
+  }
+
+  /**
    * Creates new Rect instance
    * @constructs
    */
   constructor (x1, y1, x2, y2) {
     this.pos = [x1, y1, x2, y2]
+  }
+
+  get width () {
+    return this.pos[2] - this.pos[0]
+  }
+
+  get height () {
+    return this.pos[3] - this.pos[1]
+  }
+
+  setWidth (w) {
+    this.pos[2] = this.pos[0] + w
+    return this
+  }
+
+  setHeight (h) {
+    this.pos[3] = this.pos[1] + h
+    return this
+  }
+
+  floor () {
+    this.pos = this.pos.map(Math.floor)
+    return this
+  }
+
+  ceil () {
+    this.pos = this.pos.map(Math.ceil)
+    return this
+  }
+
+  round () {
+    this.pos[0] = Math.floor(this.pos[0])
+    this.pos[1] = Math.floor(this.pos[1])
+    this.pos[2] = Math.ceil(this.pos[2])
+    this.pos[3] = Math.ceil(this.pos[3])
+    return this
+  }
+
+  _equalByRect (r) {
+    return (
+      this.pos[0] === r.pos[0] &&
+      this.pos[1] === r.pos[1] &&
+      this.pos[2] === r.pos[2] &&
+      this.pos[3] === r.pos[3]
+    )
+  }
+
+  equal (x1, y1, x2, y2) {
+    if (x1 instanceof Rect) {
+      return this._equalByRect(x1)
+    }
+
+    return (
+      this.pos[0] === x1 &&
+      this.pos[1] === y1 &&
+      this.pos[2] === x2 &&
+      this.pos[3] === y2
+    )
+  }
+
+  equals (x1, y1, x2, y2) {
+    return this.equal(x1, y1, x2, y2)
   }
 
   /**
@@ -69,6 +162,27 @@ export class Rect {
       this.pos[1] + y,
       this.pos[2] + x,
       this.pos[3] + y
+    ]
+    return this
+  }
+
+  scale (s) {
+    this.pos = [
+      this.pos[0],
+      this.pos[1],
+      this.pos[0] + (this.width * s),
+      this.pos[1] + (this.height * s)
+    ]
+
+    return this
+  }
+
+  constrict (x, y) {
+    this.pos = [
+      this.pos[0] + x,
+      this.pos[1] + y,
+      this.pos[2] - x,
+      this.pos[3] - y
     ]
     return this
   }
