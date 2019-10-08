3982be015452974f39e7c7fe5e86be6e39869e68
Matt Styles <matt@personalurban.com>
2019-10-08 08:40:10 +0100

    add seperate builds for different environments

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,67 @@
 
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 
-let pkg = require('./package.json')
+import pkg from './package.json'
 
-export default {
-  entry: 'src/index.js',
-  plugins: [
-    buble({
-      transforms: {
-        generator: false,
-        modules: false
-      }
-    })
-  ],
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: pkg.name,
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      moduleName: pkg.name,
-      sourceMap: true
-    }
-  ]
+const bubleOptions = {
+  exclude: ['node_modules/**'],
+  objectAssign: true,
+  transforms: {
+    generator: false
+  }
 }
+
+const umd = {
+  name: 'Mathutil',
+  file: 'dist/mathutil.min.js'
+}
+
+export default [
+  // umd
+  {
+    input: 'src/index.js',
+    external: [
+      ...Object.keys(pkg.dependencies)
+    ],
+    output: {
+      name: umd.name,
+      file: umd.file,
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      buble(bubleOptions),
+      terser({
+        sourcemap: true
+      }),
+      filesize()
+    ]
+  },
+  // cjs/es
+  {
+    input: 'src/index.js',
+    external: [
+      ...Object.keys(pkg.dependencies)
+    ],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    plugins: [
+      buble(bubleOptions),
+      filesize()
+    ]
+  }
+]
