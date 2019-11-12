da1e848673c39a4a8b50b8ecfeb2c9715fdb69b3
Matt Styles <matt@personalurban.com>
2019-11-12 07:45:07 +0000

    fix test case

--- spec/lerp.js
+++ spec/lerp.js
@@ -15,7 +15,7 @@ tape('lerp interpolates between positive values', t => {
 tape('lerp interpolates between negative values', t => {
   t.plan(5)
 
-  t.equals(lerp(-10, 0, 0.5), -0.5, 'negative to 0')
+  t.equals(lerp(-10, 0, 0.5), -5, 'negative to 0')
   t.equals(lerp(-20, -10, 0.5), -15, 'negative non-0')
   t.equals(lerp(-20, -10, 1.5), -5, 'can project beyond max')
   t.equals(lerp(-30, -35, -0.5), -27.5, 'can project beyond min')
