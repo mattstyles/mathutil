8888636b5634745cb8900d8456877a98f5064733
Matt Styles <matt@personalurban.com>
2019-11-12 07:44:18 +0000

    update [break] lerp can be curried

---
+++ spec/lerp.js
@@ -0,0 +1,23 @@
+
+import tape from 'tape'
+import { lerp } from '../src'
+
+tape('lerp interpolates between positive values', t => {
+  t.plan(5)
+
+  t.equals(lerp(0, 10, 0.5), 5, 'positive values from 0')
+  t.equals(lerp(2, 10, 0.5), 6, 'positive values from >0')
+  t.equals(lerp(0, 10, 1.5), 15, 'can project beyond max')
+  t.equals(lerp(0, 10, -0.5), -5, 'can project beyond min')
+  t.equals(lerp(10, 20, 0.8), 18, 'positive value')
+})
+
+tape('lerp interpolates between negative values', t => {
+  t.plan(5)
+
+  t.equals(lerp(-10, 0, 0.5), -0.5, 'negative to 0')
+  t.equals(lerp(-20, -10, 0.5), -15, 'negative non-0')
+  t.equals(lerp(-20, -10, 1.5), -5, 'can project beyond max')
+  t.equals(lerp(-30, -35, -0.5), -27.5, 'can project beyond min')
+  t.equals(lerp(-5, -15, 0.25), -7.5, 'positive value')
+})
--- src/lerp.js
+++ src/lerp.js
@@ -1,4 +1,27 @@
 
-export function lerp (value, min, max) {
+function _performLerp (value, min, max) {
   return min + value * (max - min)
+}
+
+function _curryLerp (min, max) {
+  return function lerp (value) {
+    return _performLerp(value, min, max)
+  }
+}
+
+/**
+ * Interpolates between 2 values.
+ * `value` is optional and can be curried.
+ * `value` range 0...1 corresponds to min...max, if the value falls outside of
+ * 0...1 then projection will occur.
+ * @param min <Number> the minimum boundary
+ * @param max <Number> the maximum boundary
+ * @param value <?Float> the interpolation value
+ */
+export function lerp (min, max, value) {
+  if (!value) {
+    return _curryLerp(min, max)
+  }
+
+  return _performLerp(value, min, max)
 }
