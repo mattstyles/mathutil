36e67924da7aaba563492574e992f0f667b3bcce
Matt Styles <matt@personalurban.com>
2021-12-22 08:47:08 +0000

    add swc bundle configuration

---
+++ src/clamp.ts
@@ -0,0 +1,23 @@
+function _performClamp(value: number, min: number, max: number): number {
+  return value < min ? min : value > max ? max : value
+}
+
+function _curryClamp(min: number, max: number): CurriedReturn {
+  return function clamp(value) {
+    return _performClamp(value, min, max)
+  }
+}
+
+type CurriedReturn = (value: number) => number
+
+export function clamp(
+  min: number,
+  max: number,
+  value: number
+): CurriedReturn | number {
+  if (typeof value === 'undefined') {
+    return _curryClamp(min, max)
+  }
+
+  return _performClamp(value, min, max)
+}
