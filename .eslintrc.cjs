49ad5b133c5f0cae580702fbb2b3da054b272053
Matt Styles <matt@personalurban.com>
2023-11-16 20:37:47 +0000

    update typescript to v5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.cjs'],
   root: true,
 }
